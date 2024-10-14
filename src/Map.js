a1fbd6d4689dd05746f1a177e93d330abe2f47e1
glebivanov <glebivanov@users.noreply.github.com>
2024-10-14 19:22:14 +0100

    Empty Map components added

---
+++ src/Map.js
@@ -0,0 +1,13 @@
+// Map.js
+import React from 'react';
+
+const Map = () => {
+  return (
+    <div>
+      <h1>Map Component</h1>
+      <p>This is where the map will be displayed.</p>
+    </div>
+  );
+};
+
+export default Map;
