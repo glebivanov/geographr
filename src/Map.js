e43a748f92685de00d0b134dcaa248355b7e13e3
glebivanov <glebivanov@users.noreply.github.com>
2024-10-15 19:07:53 +0100

    fixed the google maps

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,29 @@
 // Map.js
 import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100%',
+    height: '600px'
+  };
+
+  const center = {
+    lat: 51.505, // Change to your desired latitude
+    lng: -0.09   // Change to your desired longitude
+  };
 
 const Map = () => {
   return (
-    <div>
-      <h1>Map Component</h1>
-      <p>This is where the map will be displayed.</p>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyC44La52_a4nSh_0sf8qN3K_MWp1R1GQX8">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13}
+      >
+        {/* Add a marker */}
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
